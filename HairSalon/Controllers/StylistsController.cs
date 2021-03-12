f8ec4e65a8585188aff4e78fe56a562f23619798
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-12 14:05:06 -0800

    Add stylist controller that returns stylist view from database

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,23 @@
+using HairSalon.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
+  }
+}

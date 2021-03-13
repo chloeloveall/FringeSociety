5748b5a3559bdee843288b13672274a932de6d4c
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-12 18:36:03 -0800

    Add POST request to delete selected stylist and save changes

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -59,14 +59,14 @@ namespace HairSalon.Controllers
       return View(thisStylist);
     }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
-    //   _db.Stylists.Remove(thisStylist);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
   }
 }

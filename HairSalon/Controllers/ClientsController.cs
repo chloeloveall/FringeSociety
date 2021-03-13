66558a9bfbbacf9891101c1e322abc1259a4a5c0
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-12 22:17:54 -0800

    Add POST request to edit selected client and save changes

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -49,13 +49,13 @@ namespace HairSalon.Controllers
       return View(thisClient);
     }
 
-    // [HttpPost]
-    // public ActionResult Edit(Client client)
-    // {
-    //   _db.Entry(client).State = EntityState.Modified;
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
   }
 }

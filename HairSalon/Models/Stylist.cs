4cff9ab9a576f0f82b9dc4548fcd961de432bb34
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-14 16:50:48 -0700

    Add validation attributes name/phone number/email/bio/specialty

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -12,11 +12,18 @@ namespace HairSalon.Models
     }
 
     public int StylistId { get; set; }
+    [Required]
     public string StylistName { get; set; }
+    [Required]
+    [DataType(DataType.PhoneNumber)]
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
     public string StylistPhone { get; set; }
+    [Required]
     [EmailAddress]
     public string StylistEmail { get; set; }
+    [Required]
     public string StylistBio { get; set; }
+    [Required]
     public string Specialty { get; set; }
     public DateTime HireDate { get; set; }
     public virtual ICollection<Client> Clients { get; set; }

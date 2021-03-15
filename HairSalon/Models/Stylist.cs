1317770e63a99ec9b59ff5238e24ae72666cd376
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-15 00:27:20 -0700

    Add display names for auto implemented properties

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -13,18 +13,23 @@ namespace HairSalon.Models
 
     public int StylistId { get; set; }
     [Required]
+    [Display(Name="Name")]
     public string StylistName { get; set; }
     [Required]
     [DataType(DataType.PhoneNumber)]
+    [Display(Name="Phone")]
     [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
     public string StylistPhone { get; set; }
     [Required]
     [EmailAddress]
+    [Display(Name="Email")]
     public string StylistEmail { get; set; }
     [Required]
+    [Display(Name="Bio")]
     public string StylistBio { get; set; }
     [Required]
     public string Specialty { get; set; }
+    [Display(Name="Hire Date")]
     public DateTime HireDate { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
   }
